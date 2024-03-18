8135b18c5bb3e938689326b3507bbe26003c545e
Arthurhzr <160898229+Arthurhzr@users.noreply.github.com>
2024-03-18 14:58:17 -0300

    Update script_consulta.sql
    adcionado updates de data

--- db/script_consulta.sql
+++ db/script_consulta.sql
@@ -1,7 +1,56 @@
 use FrozenMeat;
 
 /*Script de seleção*/
-/*Não terminado */
+
+/*Alterando as datas de registro dos usuarios e frigorificos para datas aleatorias */
+/*Usuarios fisicos*/
+update usuario_fisico set registrado_em = '2021-05-30 22:34:21'
+	where id = 1;
+
+update usuario_fisico set registrado_em = '2019-11-23 14:45:19'
+	where id = 2;
+
+update usuario_fisico set registrado_em = '2023-07-03 20:30:40'
+	where id = 3;
+    
+update usuario_fisico set registrado_em = '2005-04-19 19:59:21'
+	where id = 4;
+    
+update usuario_fisico set registrado_em = '2018-02-23 12:42:11'
+	where id = 5;
+
+/*Usuarios juridicos*/
+update usuario_juridico set registrado_em = '2010-03-23 20:34:20'
+	where id = 1;
+
+update usuario_juridico set registrado_em = '2016-10-13 10:12:43'
+	where id = 2;
+
+update usuario_juridico set registrado_em = '2020-09-09 23:30:47'
+	where id = 3;
+    
+update usuario_juridico set registrado_em = '2009-07-11 09:49:21'
+	where id = 4;
+    
+update usuario_juridico set registrado_em = '2014-05-30 15:34:51'
+	where id = 5;
+
+/*Usuarios juridicos*/
+update frigorifico set registrado_em = '2011-04-13 10:54:30'
+	where id = 1;
+
+update frigorifico set registrado_em = '2013-11-14 15:15:23'
+	where id = 2;
+
+update frigorifico set registrado_em = '2017-01-01 22:50:47'
+	where id = 3;
+    
+update frigorifico set registrado_em = '2017-12-31 04:40:21'
+	where id = 4;
+    
+update frigorifico set registrado_em = '2012-04-10 14:34:51'
+	where id = 5;
+
 
 select 
 id as 'ID',
@@ -10,9 +59,9 @@ nome as 'Nome',
 email as 'Email', 
 cpf as 'CPF', 
 telefone as 'Numero de telefone',
-senha as 'Senha' from usuario_fisico;
+senha as 'Senha' from usuario_fisico order by registrado_em desc;
 
-select 
+select
 registrado_em as 'Data de registro', 
 nome as 'Nome', 
 email as 'Email', 
@@ -27,7 +76,7 @@ email as 'Email',
 cnpj as 'CNPJ',
 telefone as 'Numero de telefone',
 senha as 'Senha'
- from usuario_juridico;
+ from usuario_juridico order by nome;
 
 select 
 registrado_em as 'Data de Registro',
@@ -36,7 +85,7 @@ email as 'Email',
 cnpj as 'CNPJ',
 telefone as 'Numero de telefone'
  from usuario_juridico
-	where email like '%gmail%';
+	where email like '%gmail%' order by email;
     
 select 
 id as 'ID',
@@ -47,13 +96,15 @@ telefone as 'Número de telefone',
 endereco as 'Endereço',
 cep as 'CEP',
 temperatura_ideal as 'Temperatura ideal'
-from frigorifico;
+from frigorifico order by temperatura_ideal desc;
 
 
 select 
 nome as 'Nome', 
 email as 'Email', 
 telefone as 'Telefone',
-endereco as 'Endereço'
+Registrado_em as 'Data de registro'
 from frigorifico
-	where endereco like '%jardim paulista%';
+	where registrado_em like '%17%' order by registrado_em;
+    
+    
