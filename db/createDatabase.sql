da04401880c18102dbec6478d61e2823a617e878
LucasNagasse <160245061+LucasNagasse@users.noreply.github.com>
2024-03-19 13:45:08 -0300

    Update createDatabase.sql

--- db/createDatabase.sql
+++ db/createDatabase.sql
@@ -1,16 +1,3 @@
-/*
-  Grupo 8 (P.I.)
-  Integrantes:
-    Arthur Herculano Zacarias Rocha | RA: 04241063
-    Bruno Yuji Takahashi | RA: 04241032
-    Ilys Gomes Maroto | RA: 04241067
-    Juliana Freires Silva | RA: 04241064
-    Lucas Sampaio de Lucena | RA: 04241023
-    Lucas Tetsuo Nagasse | RA: 04241066
-    Marcela Gonçalves Teixeira Oliveira | RA: 04241000
-    Vitor Hideo Miyashiro Frutuoso | RA: 04241059
-*/
-
 create database FrozenMeat;
 
 use FrozenMeat;
