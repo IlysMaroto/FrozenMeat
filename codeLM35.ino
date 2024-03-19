83093b4b1da61a0c530993648ba0cea3d4a02aec
LucasLucena31 <160246500+LucasLucena31@users.noreply.github.com>
2024-03-19 11:58:19 -0300

    Update codeLM35.ino
    Adição de mínimo e maxímo

--- codeLM35.ino
+++ codeLM35.ino
@@ -1,5 +1,8 @@
 int pinoSensor = 0; // Saída do sensor na A0
+int temperaturaIMin = 0;
+int temperaturaIMax = 4;
 float temperatura = 0; // temperatura lida
+
 
 // Função que será executada uma vez (setup):
 void setup() {
@@ -8,7 +11,17 @@ void setup() {
 
 // Função executada continuamente (loop):
 void loop() {
+
+  Serial.print("Temperatura_Ideal_Miníma:");
+  Serial.print(temperaturaIMin);
+
+  Serial.print(", Temperatura_Ideal_Maxíma:");
+  Serial.print(temperaturaIMax);
+
   temperatura = (analogRead(pinoSensor) * 0.488); // 5 volts / 1.023 = 0,0048 precisão do A/D (0,0048V ~= 1ºC)
-  Serial.println (temperatura);
+  temperatura = temperatura - 25; // Ajuste da temperatura para simulação do ambiente;
+  Serial.print(", Temperatura_Frigorífico:");
+  Serial.println(temperatura);
+
   delay(500);
 }
