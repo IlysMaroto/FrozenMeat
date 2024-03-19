a46569ed97edb1c005debc07a75f7ce9a5218610
Bruno Yuji <160349419+bruno-yuji@users.noreply.github.com>
2024-03-19 10:56:41 -0300

    Update scriptInsercao.sql

--- db/scriptInsercao.sql
+++ db/scriptInsercao.sql
@@ -1,15 +1,3 @@
-/*
-  Grupo 8 (P.I.)
-  Integrantes:
-    Arthur Herculano Zacarias Rocha | RA: 04241063
-    Bruno Yuji Takahashi | RA: 04241032
-    Ilys Gomes Maroto | RA: 04241067
-    Juliana Freires Silva | RA: 04241064
-    Lucas Sampaio de Lucena | RA: 04241023
-    Lucas Tetsuo Nagasse | RA: 04241066
-    Marcela Gonçalves Teixeira Oliveira | RA: 04241000
-    Vitor Hideo Miyashiro Frutuoso | RA: 04241059
-*/
 /* Script de inserção */
 
 use FrozenMeat;
@@ -28,12 +16,12 @@ insert into usuario_juridico (nome, email, cnpj, telefone, senha, cargo) values
   ('Anna Costelas', 'chama.a.anna@gmail.com', '92458649265935', '79456783469', 'boiadeiranna123', 'Gerente'),
   ('Tourinhos Gordos', 'tourosgordosonline@hotmail.com', '94614545862354', '11946248532', 't0ur0sf0rtesepesados', 'Gerente');
 
-insert into frigorifico (nome, email, telefone, endereco, cep, temperatura_ideal) values
-  ("Swift - Alameda Lorena, 1884", "swiftresolve@swift.com.br", "08004002892", "Alameda Lorena, 1884 - Jardim Paulista, São Paulo - SP, 01424-006", "01424-006", -18),
-  ("Swift - R. Oscar Freire, 470", "swiftresolve@swift.com.br", "08004002892", "R. Oscar Freire, 470 - Cerqueira César, São Paulo - SP, 01426-000", "01426-000", -18),
-  ("Swift - R. Pamplona, 1253", "swiftresolve@swift.com.br", "08004002892", "R. Pamplona, 1253 - Jardim Paulista, São Paulo - SP, 01405-200", "01405-200", -18),
-  ("Swift - Alameda Ribeirão Preto, 580", "swiftresolve@swift.com.br", "08004002892", "Alameda Ribeirão Preto, 580 - Bela Vista, São Paulo - SP, 01331-000", "01331-000", -18),
-  ("Swift - Av. Brigadeiro Luís Antônio, 2013", "swiftresolve@swift.com.br", "08004002892", "Av. Brigadeiro Luís Antônio, 2013 - Bela Vista, São Paulo - SP, 01317-001", "01317-001", -18);
+insert into frigorifico (email, telefone, endereco, cep, temperatura_ideal) values
+  ("swiftresolve@swift.com.br", "08004002892", "Alameda Lorena, 1884 - Jardim Paulista, São Paulo - SP, 01424-006", "01424-006", -18),
+  ("swiftresolve@swift.com.br", "08004002892", "R. Oscar Freire, 470 - Cerqueira César, São Paulo - SP, 01426-000", "01426-000", -18),
+  ("swiftresolve@swift.com.br", "08004002892", "R. Pamplona, 1253 - Jardim Paulista, São Paulo - SP, 01405-200", "01405-200", -18),
+  ("swiftresolve@swift.com.br", "08004002892", "Alameda Ribeirão Preto, 580 - Bela Vista, São Paulo - SP, 01331-000", "01331-000", -18),
+  ("swiftresolve@swift.com.br", "08004002892", "Av. Brigadeiro Luís Antônio, 2013 - Bela Vista, São Paulo - SP, 01317-001", "01317-001", -18);
 
 insert into usuario_fisico_frigorifico (id_usuario, id_frigorifico) values
   (1, 1),
@@ -54,8 +42,8 @@ insert into usuario_juridico_frigorifico (id_usuario, id_frigorifico) values
   (5, 5);
 
 insert into historico_frigorifico (id_frigorifico, temperatura) values
-  (1, -16),
-  (2, -18),
-  (3, -13),
-  (4, -20),
-  (5, -17);
+  (1, 0),
+  (2, 3),
+  (3, 2.5),
+  (4, 1.7),
+  (5, 5);
